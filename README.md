47b7351f358743120f1b0667dbea7280dc3e7549
Ingo Oppermann <ingo@oppermann.ch>
2019-09-25 21:50:36 +0200

    Add weather display, more config options

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # clappr-weather
 
-Still work in progress.
+Displays the current weather information in an overlay at the top of the player.
 
 ## What is there already?
 
 - An element is created that can be used to fill in the data (`src/public/weather.html` and `src/public/style.scss`)
 - Reading out the station ID from the config
 - XHR request to load the data
+- Displaying the weather data
 
 ## TODO
 
-- The station data should not be a JSONP response
+- ICONS!!!!
+- The station data should not be a JSONP response. There is a workaround.
 - Add some proper CORS header to the station server, otherwise is will not load from localhost
-- Displaying the weather data
-- The data should only displayed when the stream is playing
 
 ## Config
 
@@ -24,9 +24,33 @@ var player = new Clappr.Player({
 	plugins: [Weather],
 
 	weatherConfig: {
-		station: 82
+		station: 38
 	},
 
 	...
 });
 ```
+
+By default it tries to fetch the data from `https://livespotting.com/weather/live/[station].json`. You can override this
+by providing an alternative URL in the `weatherConfig`:
+
+```
+weatherConfig: {
+	station: 38,
+	url: 'https://example.com/weather/[station].json'
+},
+```
+
+The placeholder `[station]` will be replaced by the value of `station`.
+
+By default the weather data will be updated ever 5 seconds. You can override this by providing an alternative interval
+in the `weatherConfig`:
+
+```
+weatherConfig: {
+	station: 38,
+	refresh: 15
+},
+```
+
+This will refresh the weather data every 15 seconds.
--- src/main.js
+++ src/main.js
@@ -2,31 +2,39 @@ import { Events, Styler, UICorePlugin, UIContainerPlugin, template } from 'clapp
 import pluginHtml from './public/weather.html';
 import pluginStyle from './public/style.scss';
 
-const VERSION = '1.0.0';
+const VERSION = '0.1.0';
+const DEFAULT_URL = 'https://livespotting.com/weather/live/[station].json';
+const DEFAULT_REFRESH = 5;
 
 export default class Weather extends UICorePlugin {
-	constructor(container) {
-	    super(container);
+	bindEvents() {
+    	this.listenTo(this.core, Events.CORE_ACTIVE_CONTAINER_CHANGED, () => { this.core.activeContainer.$el.append(this.el); });
+    	this.listenToOnce(this.core, Events.CORE_READY, this.onCoreReady);
+	}
 
-	    this.hasData = false;
+	onCoreReady() {
+		this.hasData = false;
+		this.isPlaying = true;
+
 	    this.station = 0;
-
-	    this.$el.html(this.template());
-	    this.$el.append(Styler.getStyleFor(pluginStyle));
+	    this.url = DEFAULT_URL;
+	    this.refresh = DEFAULT_REFRESH;
 
 	    const cfg = this.core.options.weatherConfig || {};
-
-	    console.log(cfg);
 
 		if('station' in cfg) {
 			this.station = cfg.station;
 		}
 
-	    this.loadData();
-	}
+		if('url' in cfg) {
+			this.url = cfg.url;
+		}
 
-	bindEvents() {
-    	this.listenTo(this.core, Events.CORE_ACTIVE_CONTAINER_CHANGED, () => { this.core.activeContainer.$el.append(this.el); });
+		if('refresh' in cfg) {
+			this.refresh = parseInt(cfg.refresh);
+		}
+
+	    this.loadData();
 	}
 
 	static get version() {
@@ -53,10 +61,30 @@ export default class Weather extends UICorePlugin {
 	}
 
 	setData(data) {
+		if(data === null) {
+			return;
+		}
+
 		this.data = data;
 		this.hasData = true;
 
-		this.$el.show();
+		this.$el.html(this.template({
+			temperature: data.temperature,
+			rain: data.rain,
+			winddirection: data.winddirection_txt,
+			windspeed: data['windspeed-ms']
+		}));
+	    this.$el.append(Styler.getStyleFor(pluginStyle));
+
+		this.show();
+	}
+
+	show() {
+		if(this.hasData == true && this.isPlaying == true) {
+			this.$el.show();
+		}
+
+		return;
 	}
 
 	loadData() {
@@ -67,19 +95,36 @@ export default class Weather extends UICorePlugin {
 		let self = this;
 
 		let r = new XMLHttpRequest();
-		r.open("GET", "https://livespotting.com/weather/live/" + this.station + ".json");
+
+		let url = this.url.replace('[station]', this.station);
+		r.open("GET", url);
 		r.onload = function() {
 			if(r.readyState === r.DONE) {
 		        if(r.status === 200) {
-		            console.log(r.response);
-		            console.log(r.responseText);
+		        	let text = r.responseText;
 
-		            self.setData(r.responseText);
+		        	if(text.startsWith('lscallback({')) {
+		        		text = text.replace('lscallback({', '{');
+		        		text = text.replace('})', '}');
+		        	}
+
+		        	let data = null;
+		        	try {
+		            	data = JSON.parse(text);
+		            } catch(error) {
+		            	console.log(error);
+		            	console.log(r.responseText);
+		            	data = null;
+		            }
+
+		            self.setData(data);
 		        }
 
-		        setTimeout(function() {
-			    	self.loadData();
-			    }, 1000);
+		        if(self.refresh > 0) {
+			        setTimeout(function() {
+				    	self.loadData();
+				    }, self.refresh * 1000);
+			    }
 		    }
 		};
 		r.send();
