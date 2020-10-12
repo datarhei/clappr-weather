84d97e7582e9748fb2b51c857724f07e80931d07
Ingo Oppermann <ingo@oppermann.ch>
2020-10-12 16:03:20 +0200

    More flexible configuration, unit transformation

--- README.md
+++ README.md
@@ -5,15 +5,13 @@ Displays the current weather information in an overlay at the top of the player.
 ## What is there already?
 
 - An element is created that can be used to fill in the data (`src/public/weather.html` and `src/public/style.scss`)
-- Reading out the station ID from the config
-- XHR request to load the data
-- Displaying the weather data
+- XHR request to load the data.
+- Displaying the weather data.
+- Weather data is only displayed when playing and hides when stopped.
 
 ## TODO
 
 - ICONS!!!!
-- The station data should not be a JSONP response. There is a workaround.
-- Add some proper CORS header to the station server, otherwise is will not load from localhost
 
 ## Config
 
@@ -24,33 +22,111 @@ var player = new Clappr.Player({
 	plugins: [Weather],
 
 	weatherConfig: {
-		station: 38
+		url: 'https://example.com/weather/data.json'
 	},
 
 	...
 });
 ```
 
-By default it tries to fetch the data from `https://livespotting.com/weather/live/[station].json`. You can override this
-by providing an alternative URL in the `weatherConfig`:
-
-```
-weatherConfig: {
-	station: 38,
-	url: 'https://example.com/weather/[station].json'
-},
-```
-
-The placeholder `[station]` will be replaced by the value of `station`.
-
 By default the weather data will be updated ever 5 seconds. You can override this by providing an alternative interval
 in the `weatherConfig`:
 
 ```
 weatherConfig: {
-	station: 38,
-	refresh: 15
+	url: 'https://example.com/weather/data.json',
+	refresh: 60
 },
 ```
 
-This will refresh the weather data every 15 seconds.
+This will refresh the weather data every 60 seconds.
+
+By default the weather data is displayed in metric units. You can override this by setting the value for `units` to `imperial`:
+
+```
+weatherConfig: {
+	url: 'https://example.com/weather/data.json',
+	units: 'imperial'
+},
+```
+
+## Data format
+
+The expected data format for the weather data has to be JSON and has the form
+
+```json
+{
+	"windspeed_ms": 1.5,
+	"winddirection": 253,
+	"temperature_c": 10.5,
+	"humidity": 81,
+	"pressure_hpa": 1021.77,
+	"precipitation": {
+		"value_mm": 0.0,
+		"mode": "rain",
+	},
+	"timestamp": 1602503869,
+}
+```
+
+The values are expected to be in the metric system.
+
+> Vincent: "No man, they got the metric system. They wouldn't know what the f**k a quarter pounder is."
+> Jules: "Then what do they call it?"
+> Vincent: "They call it a Royale with cheese."
+
+Field | Unit | Description
+------|------|------------
+`windspeed_ms` | `m/s` | The wind speed in meters per second.
+`winddirection` | `°` | The direction the wind is coming from. Values from `0` to `359`.
+`temperature_c` | `°C` | The temperature in degress Celsius.
+`humidity` | `%` | The relative air humidity.
+`pressure_hpa` | `hpa` | The atmospheric pressure in hpa.
+`precipitation.value_mm` | `mm` | The precipitation in millimeter.
+`precipitation.mode` | | One value of `none`, `rain`, `snow`.
+`timestamp` | | Unix timestamp of the measurement.
+
+If the server where you load the data from delivers the data in a different format, you can define a rewrite function in the config
+that will transform the output into the expected form. The function expects one parameter which is a JavaScript object and will return
+an object in the above mentioned form. The default for the rewrite function is `null` which means that no rewrite will happen.
+
+Here is an example that rewrites the data from openweathermap.org:
+
+```js
+var player = new Clappr.Player({
+	...
+
+	plugins: [Weather],
+
+	weatherConfig: {
+		url: 'https://example.com/weather/data.json',
+		rewrite: function(data) {
+			var d = {
+				"windspeed_ms": data.wind.speed,
+				"winddirection": data.wind.deg,
+				"temperature_c": data.main.temp,
+				"humidity": data.main.humidity,
+				"pressure_hpa": data.main.pressure,
+				"precipitation": {
+					"value_mm": 0.0,
+					"mode": "none",
+				},
+				"timestamp": data.dt
+			};
+
+			if ('rain' in data) {
+				d.precipitation.value_mm = data.rain.rain_1h;
+				d.precipitation.mode = "rain";
+			}
+			else if ('snow' in data) {
+				d.precipitation.value_mm = data.snow.snow_1h;
+				d.precipitation.mode = "snow";
+			}
+
+			return d;
+		}
+	},
+
+	...
+});
+```
--- src/main.js
+++ src/main.js
@@ -1,40 +1,63 @@
-import { Events, Styler, UICorePlugin, UIContainerPlugin, template } from 'clappr';
+import { Events, Styler, UICorePlugin, template } from 'clappr';
 import pluginHtml from './public/weather.html';
 import pluginStyle from './public/style.scss';
 
 const VERSION = '0.1.0';
-const DEFAULT_URL = 'https://livespotting.com/weather/live/[station].json';
+const DEFAULT_URL = null;
 const DEFAULT_REFRESH = 5;
+const DEFAULT_REWRITE = null;
+const DEFAULT_UNITS = 'metric';
 
 export default class Weather extends UICorePlugin {
 	bindEvents() {
-    	this.listenTo(this.core, Events.CORE_ACTIVE_CONTAINER_CHANGED, () => { this.core.activeContainer.$el.append(this.el); });
-    	this.listenToOnce(this.core, Events.CORE_READY, this.onCoreReady);
+		this.listenTo(this.core, Events.CORE_ACTIVE_CONTAINER_CHANGED, () => {
+			this.core.activeContainer.$el.append(this.el);
+
+			this.playback = this.core.getCurrentPlayback();
+			this.listenTo(this.playback, Events.PLAYBACK_PLAY, () => {
+				this.isPlaying = true;
+				this.show();
+			});
+			this.listenTo(this.playback, Events.PLAYBACK_STOP, () => {
+				this.isPlaying = false;
+				this.show();
+			});
+		});
+		this.listenToOnce(this.core, Events.CORE_READY, this.onCoreReady);
 	}
 
 	onCoreReady() {
 		this.hasData = false;
-		this.isPlaying = true;
+		this.isPlaying = false;
 
-	    this.station = 0;
-	    this.url = DEFAULT_URL;
-	    this.refresh = DEFAULT_REFRESH;
+		this.url = DEFAULT_URL;
+		this.rewrite = DEFAULT_REWRITE;
+		this.refresh = DEFAULT_REFRESH;
+		this.units = DEFAULT_UNITS;
 
-	    const cfg = this.core.options.weatherConfig || {};
+		const cfg = this.core.options.weatherConfig || {};
 
-		if('station' in cfg) {
-			this.station = cfg.station;
+		if ('rewrite' in cfg) {
+			if (typeof cfg.rewrite === 'function') {
+				this.rewrite = cfg.rewrite;
+			}
 		}
 
-		if('url' in cfg) {
+		if ('url' in cfg) {
 			this.url = cfg.url;
 		}
 
-		if('refresh' in cfg) {
+		if ('refresh' in cfg) {
 			this.refresh = parseInt(cfg.refresh);
 		}
 
-	    this.loadData();
+		if ('units' in cfg) {
+			if (cfg.units === 'imperial') {
+				this.units = 'imperial';
+			}
+		}
+
+		this.loadData();
 	}
 
 	static get version() {
@@ -61,34 +84,94 @@ export default class Weather extends UICorePlugin {
 	}
 
 	setData(data) {
-		if(data === null) {
+		if (data === null) {
 			return;
 		}
 
 		this.data = data;
 		this.hasData = true;
 
+		let temperature = undefined;
+		let precipitation = undefined;
+		let winddirection = undefined;
+		let windspeed = undefined;
+		let pressure = undefined;
+		let humidity = undefined;
+		let timestamp = undefined;
+
+		if(this.units === 'imperial') {
+			temperature = this._convertToFahrenheit(data.temperature_c).toFixed(1) + ' °F';
+			winddirection = data.winddirection.toFixed(0) + '° ' + this._getHeading(data.winddirection);
+			windspeed = this._convertToMPH(data.windspeed_ms).toFixed(1) + ' mph';
+		}
+		else {
+			temperature = data.temperature_c.toFixed(1) + ' °C';
+			winddirection = data.winddirection.toFixed(0) + '° ' + this._getHeading(data.winddirection);
+			windspeed = data.windspeed_ms.toFixed(1) + ' m/s';
+		}
+
+		precipitation = data.precipitation.value_mm.toFixed(1) + ' mm';
+		pressure = data.pressure_hpa.toFixed(2) + ' hpa';
+		humidity = data.humidity.toFixed(0) + '%';
+
 		this.$el.html(this.template({
-			temperature: data.temperature.toFixed(1),
-			rain: data.rain.toFixed(1),
-			winddirection: data.winddirection + '° ' + data.winddirection_txt,
-			windspeed: data['windspeed-ms']
+			temperature: temperature,
+			precipitation: precipitation,
+			winddirection: winddirection,
+			windspeed: windspeed,
+			pressure: pressure,
+			humidity: humidity,
+			timestamp: timestamp,
 		}));
-	    this.$el.append(Styler.getStyleFor(pluginStyle));
+		this.$el.append(Styler.getStyleFor(pluginStyle));
 
 		this.show();
 	}
 
+	_convertToFahrenheit(_value) {
+		return (_value * 1.8) + 32;
+	}
+
+	_convertToMPH(_value) {
+		return (_value / 1.609344 * 1000 / 3600);
+	}
+
+	_getHeading(_direction) {
+		if(_direction < 0 || _direction > 360) {
+			return '--';
+		}
+
+		const directions = 'N NNE NE ENE E ESE SE SSE S SSW SW WSW W WNW NW NNW'.split(' ');
+
+		let direction = 0;
+		const step = Math.floor(360 / directions.length);
+		let i = Math.floor(step / 2);
+
+		if(_direction <= i || _direction >= 360 - i) {
+			return 'N';
+		}
+
+		while (i <= _direction) {
+			direction++;
+			i += step;
+		}
+
+		return directions[direction];
+	}
+
 	show() {
-		if(this.hasData == true && this.isPlaying == true) {
+		if (this.hasData == true && this.isPlaying == true) {
 			this.$el.show();
+		}
+		else {
+			this.$el.hide();
 		}
 
 		return;
 	}
 
 	loadData() {
-		if(!this.station) {
+		if (!this.url) {
 			return;
 		}
 
@@ -96,36 +179,32 @@ export default class Weather extends UICorePlugin {
 
 		let r = new XMLHttpRequest();
 
-		let url = this.url.replace('[station]', this.station);
-		r.open("GET", url);
+		r.open('GET', this.url);
 		r.onload = function() {
-			if(r.readyState === r.DONE) {
-		        if(r.status === 200) {
-		        	let text = r.responseText;
+			if (r.readyState === r.DONE) {
+				if (r.status === 200) {
+					let text = r.responseText;
 
-		        	if(text.startsWith('lscallback({')) {
-		        		text = text.replace('lscallback({', '{');
-		        		text = text.replace('})', '}');
-		        	}
+					let data = null;
+					try {
+						data = JSON.parse(text);
+					} catch (error) {
+						data = text;
+					}
 
-		        	let data = null;
-		        	try {
-		            	data = JSON.parse(text);
-		            } catch(error) {
-		            	console.log(error);
-		            	console.log(r.responseText);
-		            	data = null;
-		            }
+					if (self.rewrite !== null) {
+						data = self.rewrite(data);
+					}
 
-		            self.setData(data);
-		        }
+					self.setData(data);
+				}
 
-		        if(self.refresh > 0) {
-			        setTimeout(function() {
-				    	self.loadData();
-				    }, self.refresh * 1000);
-			    }
-		    }
+				if (self.refresh > 0) {
+					setTimeout(function() {
+						self.loadData();
+					}, self.refresh * 1000);
+				}
+			}
 		};
 		r.send();
 	}
